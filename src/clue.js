78e6140d043370ef5d6915eb790396d228b7cc16
Ali Amidpour <ali.amidpour.eng@gmail.com>
2022-02-16 03:19:38 +0100

    done

--- src/clue.js
+++ src/clue.js
@@ -2,20 +2,54 @@
 
 // Suspects Array
 
-const suspectsArray = [];
+const suspectsArray = [
+  firstName: Jacob,
+  lastName: Green,
+  occupation: Entrepreneur,
+  age: 45,
+  description: He has a lot of connections,
+  image: https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg ,
+  color: green];
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [[{name: "Dining Room"],
+  [name: "Conservatory"],
+  [name: "Kitchen"],
+  [name: "Study"],
+  [name: "Library"],
+  [name: "Billiard Room"],
+  [name: "Lounge"],
+  [name: "Ballroom"],
+  [name: "Hall"],
+  [name: "Spa"],
+  [name: "Living Room"],
+  [name: "Observatory"],
+  [name: "Theater"],
+  [name: "Guest House"],
+  [name: "Patio"]}];
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [{[name: rope --- weight: 10],
+  [name: "knife" , weight: 8],
+  [name: "candlestick" , weight: 2],
+  [name: "dumbbell" , weight: 30],
+  [name: "poison" , weight: 2],
+  [name: "axe" , weight: 15],
+  [name: "bat" , weight: 13],
+  [name: "trophy" , weight: 25],
+  [name: "pistol" , weight: 20]}];
 
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom() {
+  const rm = Math.floor(Math.random() * roomsArray.length) +1 ;
+  const rnm = Math.floor(Math.random() * weaponsArray.length) +1 ;
+  return console.log(`${suspectsArray.name} was in ${roomsArray[rm]} had 
+  ${weaponsArray[rnm].name} and his weapon's weight was ${weaponsArray[rnm].weaponsArray} `)
+}
 
 function pickMystery() {}
 
